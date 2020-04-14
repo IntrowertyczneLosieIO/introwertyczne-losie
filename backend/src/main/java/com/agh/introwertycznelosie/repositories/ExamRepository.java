66ad5dcbdb33debd849122a49f12bd7e0420f2f0
Tom <bloodsnow5114@gmail.com>
2020-04-14 17:13:18 +0200

    IL-4

--- backend/src/main/java/com/agh/introwertycznelosie/repositories/ExamRepository.java
+++ backend/src/main/java/com/agh/introwertycznelosie/repositories/ExamRepository.java
@@ -3,7 +3,6 @@ package com.agh.introwertycznelosie.repositories;
 import com.agh.introwertycznelosie.data.Exam;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
 
 public interface ExamRepository extends JpaRepository<Exam, Long> {
 }
