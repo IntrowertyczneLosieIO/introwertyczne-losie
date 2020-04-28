29c22181ad92aa27ddaeb1ffb84d9c6cded50401
magk98 <magk98@poczta.fm>
2020-04-28 18:35:02 +0200

    subexam changed

--- backend/src/test/java/com/agh/introwertycznelosie/SubexamTests.java
+++ backend/src/test/java/com/agh/introwertycznelosie/SubexamTests.java
@@ -9,11 +9,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.persistence.EntityNotFoundException;
 import java.time.LocalTime;
 import java.util.Date;
 import java.util.LinkedList;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
 public class SubexamTests {
