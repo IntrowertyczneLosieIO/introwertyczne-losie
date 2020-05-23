a81997bf922e52d81b4713b0d0511e62a78b8dd0
Tom <bloodsnow5114@gmail.com>
2020-05-23 19:58:45 +0200

    IL-37

--- backend/src/main/java/com/agh/introwertycznelosie/config/LoginController.java
+++ backend/src/main/java/com/agh/introwertycznelosie/config/LoginController.java
@@ -1,5 +1,6 @@
 package com.agh.introwertycznelosie.config;
 
+import com.agh.introwertycznelosie.data.RegistrationToken;
 import com.agh.introwertycznelosie.data.Role;
 import com.agh.introwertycznelosie.data.User;
 import com.agh.introwertycznelosie.repositories.UserRepository;
@@ -11,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -20,6 +22,7 @@ import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.HashSet;
+import java.util.UUID;
 
 @Controller
 public class LoginController {
@@ -33,11 +36,9 @@ public class LoginController {
     UserService userService;
     @Autowired
     UserRepository userRepository;
+    @Autowired
+    EmailSenderService emailSenderService;
 
-    @GetMapping("/home")
-    public String home() {
-        return "home";
-    }
 
     @RequestMapping(value = "/getCurrentUserRole", method = RequestMethod.GET)
     public String getRole() {
@@ -58,13 +59,37 @@ public class LoginController {
     }
 
     @RequestMapping(value = "/register", method = RequestMethod.PUT)
+    public ResponseEntity<HttpStatus> addUser(@RequestBody User user) {
+        RegistrationToken registrationToken = new RegistrationToken();
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(user.getUsername());
+        mailMessage.setSubject("Complete Registration!");
+        mailMessage.setFrom("introwertycznelosie@gmail.com");
+        mailMessage.setText("Your registration "
+                    +"http://localhost:8080/registration?token="+registrationToken.getToken());
+        emailSenderService.sendEmail(mailMessage);
+        return ResponseEntity.ok(HttpStatus.OK);
+
+    }
+
+    @RequestMapping(value = "/registration", method = RequestMethod.GET)
+    public ModelAndView registerUser(@RequestParam("token") String registrationToken) {
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("../static/index");
+        return modelAndView;
+
+    }
+
+
+
+    @RequestMapping(value = "/register", method = RequestMethod.PUT)
     public ResponseEntity<HttpStatus> register(@RequestBody User user) {
         user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
         userRepository.save(user);
         return ResponseEntity.ok(HttpStatus.OK);
 
     }
-
 
     @RequestMapping(value = {"/*"}, method = RequestMethod.GET)
     public ModelAndView index() {
---
+++ backend/src/main/java/com/agh/introwertycznelosie/data/RegistrationToken.java
@@ -0,0 +1,70 @@
+package com.agh.introwertycznelosie.data;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.UUID;
+
+@Entity
+public class RegistrationToken {
+    private static final int EXPIRATION = 60 * 24;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String token;
+
+    private String email;
+
+    private Date expiryDate;
+
+    public RegistrationToken() {
+    }
+
+    private Date calculateExpiryDate(int expiryTimeInMinutes) {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(new Timestamp(cal.getTime().getTime()));
+        cal.add(Calendar.MINUTE, expiryTimeInMinutes);
+        return new Date(cal.getTime().getTime());
+    }
+
+    public RegistrationToken(String email) {
+        this.token =  UUID.randomUUID().toString();
+        this.email = email;
+        this.expiryDate = calculateExpiryDate(EXPIRATION);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getExpiryDate() {
+        return expiryDate;
+    }
+
+    public void setExpiryDate(Date expiryDate) {
+        this.expiryDate = expiryDate;
+    }
+}
