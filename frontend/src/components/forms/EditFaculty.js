167136bf72bf32df848416c75ea2ec1d7929b115
albertRk <royptb15@gmail.com>
2020-06-02 21:47:53 +0200

    [IL-61] Edytowanie wydziałów

---
+++ frontend/src/components/forms/EditFaculty.js
@@ -0,0 +1,152 @@
+import React from "react";
+import Modal from "react-bootstrap/Modal";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import NewFacultyInfo from "./formParts/NewFacultyInfo";
+
+class EditFaculty extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.formRef = React.createRef();
+        this.state = {
+            validated: false,
+            showConfirmationModal: false,
+            userData: this.getInitialState()
+        };
+    }
+
+    setShowConfirmationModal = (show) => {
+        this.setState({
+            showConfirmationModal: show
+        });
+    }
+
+    handleCloseConfirmationModal = () => this.setShowConfirmationModal(false);
+    handleOpenConfirmationModal = () => this.setShowConfirmationModal(true);
+
+    handleSaveAndOpenConfirm = () => {
+        let promise = new Promise(this.handleSave);
+        promise.then(
+            this.handleOpenConfirmationModal
+        ).catch(
+            () => {
+                console.log("err")
+            }
+        )
+    }
+
+    setValidated = (validated) => {
+        this.setState({
+            validated
+        });
+    }
+
+    getInitialState = () => {
+        if (this.props.initialInputValues) {
+            return this.props.initialInputValues;
+        } else {
+            return {
+                name: "",
+                acronym: ""
+            }
+        }
+    }
+
+    getFormData = (target, value) => {
+        let currentUserData = this.state.userData;
+        this.setState({
+            userData: {
+                ...currentUserData,
+                [target]: value
+            }
+        })
+    }
+
+    handleInputChange = (event) => {
+        let currentUserData = this.state.userData;
+        this.setState({
+            userData: {
+                ...currentUserData,
+                [event.target.id]: event.target.value
+            }
+        });
+    }
+
+    handleSave = (resolve, reject) => {
+        let form = this.formRef.current;
+        if (!form.checkValidity()) {
+            this.setValidated(true);
+            reject();
+        } else {
+            this.props.handleHide();
+            let userDataToSend = {
+                name: this.state.userData.name,
+                acronym: this.state.userData.acronym
+            }
+            fetch(`/edit-faculty/${this.props.initialInputValues.id}`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(userDataToSend)
+            })
+                .then((response) => response.json())
+                .then(() => {
+                    this.setState({
+                        userData: this.getInitialState()
+                    });
+                    this.setValidated(false);
+                    resolve();
+                })
+                .catch((error) => console.log(error));
+        }
+    }
+
+    hideAndClearState = () => {
+        this.setState({
+            userData: this.getInitialState()
+        });
+        this.setValidated(false);
+        this.props.handleHide();
+    }
+
+
+    render() {
+        return (
+            <>
+                <Modal show={this.props.show} dialogClassName={"custom-width-modal"} onHide={this.hideAndClearState}
+                       backdrop={"static"} keyboard={false}>
+                    <Modal.Header closeButton className={"modal-form-bg-color"}>
+                        <Modal.Title className={"custom-margins custom-font text-light"}>Edytowanie
+                            wydziału</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body className={"custom-margins"}>
+                        <Form noValidate validated={this.state.validated} ref={this.formRef}>
+                            <NewFacultyInfo getFormData={this.getFormData} inputValuesFromState={this.state.userData}/>
+                        </Form>
+                    </Modal.Body>
+                    <Modal.Footer className={"modal-form-bg-color"}>
+                        <Button variant={"danger"} onClick={this.hideAndClearState}>Anuluj
+                        </Button>
+                        <Button variant={"success"} className={"custom-margins"}
+                                onClick={this.handleSaveAndOpenConfirm}>
+                            Edytuj wydział
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
+                <Modal show={this.state.showConfirmationModal} onHide={this.handleCloseConfirmationModal} size={"lg"}>
+                    <Modal.Body>
+                        <h4 className={"text-center"}>Wydział został edytowany pomyślnie.</h4>
+                    </Modal.Body>
+                    <Modal.Footer className={"modal-form-bg-color"}>
+                        <Button variant={"success"} onClick={this.handleCloseConfirmationModal} block
+                                size={"sm"}>OK</Button>
+                    </Modal.Footer>
+                </Modal>
+            </>
+        );
+    }
+}
+
+export default EditFaculty;
